8a956564b7aa21f30c10d9f965ba6d9c1d6aeb06
Federico Galeano <fede.n.gal@hotmail.com>
2018-01-13 19:11:31 -0300

    pass new Game to the new Window

--- src/fede/Main.java
+++ src/fede/Main.java
@@ -5,6 +5,6 @@ import javax.swing.SwingUtilities;
 public class Main {
 	
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> new Window());
+		SwingUtilities.invokeLater(() -> new Window(new Game()));
 	}
 }
