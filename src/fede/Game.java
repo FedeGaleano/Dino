842089997dd111f78c0477ae5443327d04df0072
Federico Galeano <fede.n.gal@hotmail.com>
2018-01-13 19:38:00 -0300

    Ensure background and foreground colors

--- src/fede/Game.java
+++ src/fede/Game.java
@@ -1,13 +1,19 @@
 package fede;
 
 import java.awt.Canvas;
+import java.awt.Color;
 
 @SuppressWarnings("serial")
 public class Game extends Canvas {
+	
+	private static final Color foregroundColor = Color.decode("#535353");
+	private static final Color backgroundColor = Color.decode("#F7F7F7");
 	
 	public Game() {
 		super();
 		this.setIgnoreRepaint(true);
 		this.setSize(600, 200);
+		this.setBackground(backgroundColor);
+		this.setForeground(foregroundColor);
 	}
 }
