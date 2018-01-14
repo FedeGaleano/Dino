342034b5c0c36c8a85fdeec7485a4322cc9c4b4a
Federico Galeano <fede.n.gal@hotmail.com>
2018-01-13 21:53:27 -0300

    render dinosaur image

--- src/fede/Dinosaur.java
+++ src/fede/Dinosaur.java
@@ -1,16 +1,34 @@
 package fede;
 
 import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Dinosaur {
 	
 	private Graphics g;
+	private Image image;
+	
+	public Dinosaur() {
+		try
+		{
+			image = ImageIO
+					.read(new File("res/Dinosaur.png"))
+					.getSubimage(0, 0, 40, 42);
+		}
+		catch(IOException e)
+		{
+			System.out.println("Couldn't open dinosaur spritesheet file");
+		}
+	}
 	
 	public void setGraphics(Graphics g) {
 		this.g = g;
 	}
 	
 	public void render() {
-		g.fillRect(100, 100, 20, 50);
+		g.drawImage(image, 100, 100, null);
 	}
 }
