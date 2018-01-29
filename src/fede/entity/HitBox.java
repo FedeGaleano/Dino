a47f925b39212cc1feed62aed4b05991353dc5c0
Federico Galeano <fede.n.gal@hotmail.com>
2018-01-29 17:24:43 -0300

    enhance hitbox collision algorithm

--- src/fede/entity/HitBox.java
+++ src/fede/entity/HitBox.java
@@ -1,6 +1,8 @@
 package fede.entity;
 
 import java.awt.Point;
+import static java.lang.Math.abs;
+import static java.lang.Math.round;
 
 public class HitBox {
 	public int x0, y0, delta_x, delta_y;
@@ -21,10 +23,10 @@ public class HitBox {
 	}
 	
 	public boolean collidesInX(HitBox another) {
-		return Math.abs(this.x0 + this.delta_x / 2 - (another.x0 + another.delta_x / 2)) <= (this.delta_x + another.delta_x) / 2;
+		return abs(round(this.x0 + this.delta_x / 2.0) - round(another.x0 + another.delta_x / 2.0)) <= round((this.delta_x + another.delta_x) / 2.0);
 	}
 	
 	public boolean collidesInY(HitBox another) {
-		return Math.abs(this.y0 + this.delta_y / 2 - (another.y0 + another.delta_y / 2)) <= (this.delta_y + another.delta_y) / 2;
+		return abs(round(this.y0 + this.delta_y / 2.0) - round(another.y0 + another.delta_y / 2.0)) <= round((this.delta_y + another.delta_y) / 2.0);
 	}
 }
